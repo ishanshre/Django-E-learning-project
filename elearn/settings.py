560324f657d67376704250ceebcc673d3b63ca46
Ishan Shrestha <ishan.shrestha356@gmail.com>
2022-12-31 08:55:04 +0545

    feat: create custom user model profile model

--- elearn/settings.py
+++ elearn/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'phonenumber_field',
 
     # local app
+    'user.apps.UserConfig',
 ]
 
 MIDDLEWARE = [
@@ -139,7 +140,7 @@ MEDIA_ROOT = BASE_DIR / "media"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
+AUTH_USER_MODEL = "user.User"
 
 
 
---
+++ user/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+
+from phonenumber_field.modelfields import PhoneNumberField
+
+from ckeditor_uploader.fields import RichTextUploadingField
+
+from user.countries import COUNTRIES_CHOOSE
+# Create your models here.
+
+
+class User(AbstractUser):
+    email = models.EmailField(_("Email Address"),max_length=255, unique=True)
+    date_of_birth = models.DateTimeField(null=True, blank=True)
+    email_verified = models.BooleanField(default=False)
+
+class Profile(models.Model):
+    class GENDER(models.TextChoices):
+        MALE = "MALE", 'MALE'
+        FEMALE = "FEMALE", 'FEMALE'
+        OTHERS = "OTHERS", 'OTHERS'
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    avatar = models.ImageField(upload_to="user/profile/avatar", null=True, blank=True)
+    bio = RichTextUploadingField(null=True, blank=True)
+    country = models.CharField(max_length=32, choices=COUNTRIES_CHOOSE.choices, null=True, blank=True)
+    phone = PhoneNumberField(blank=True, null=True)
+    github = models.URLField(max_length=255, null=True, blank=True)
+    twitter = models.URLField(max_length=255, null=True, blank=True)
+    linkedin = models.URLField(max_length=255, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username.title()} Profile"
+    

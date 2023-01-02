e876bf9131dff66c24860e7254863dbf7dc1b66b
Ishan Shrestha <ishan.shrestha356@gmail.com>
2023-01-02 12:16:55 +0545

    feat: create mange course list view

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,5 @@ from core import views
 app_name = "core"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("courses/manage", views.ManageCourseListView.as_view(), name="manage_course_list"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from core.models import Course
+
 
 # Create your views here.
 def index(request):
     return render(request, "index.html")
+
+
+class ManageCourseListView(LoginRequiredMixin, ListView):
+    model = Course
+    context_object_name = "my_courses"
+    template_name = "courses/manage/course/list.html"
+
+    def get_queryset(self):
+        query = super().get_queryset()
+        return query.filter(owner=self.request.user)
+    
